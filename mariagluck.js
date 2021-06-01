9cde92a3a5f9952fe9802986e4d07caacd45b8e0
mariagluck <mariamart.berlin@gmail.com>
2021-06-01 23:40:16 +0200

    questions

--- mariagluck.js
+++ mariagluck.js
@@ -4,96 +4,29 @@
 // Film: "Harry Potter" for 11 years old up to 15 years old.
 // print message for clients older than 15 years old.
 
-let age = 10;
-    if (age >= 0 && age <= 5) {
-    console.log("The film for you today is PONYO!");
-  } else  if (age >= 6 && age <= 10) {
-    console.log("The film for you today is My Neighbor Totoro!");
-  } else  if (age >= 11 && age <= 15) {
-        console.log("The film for you today is Harry Potter!");
-  } else {
-      console.log("You are too old. Go to another cinema!!")
-  }
+
 //  e.g. if age = 10 --> it prints: -->  "The film for you today is My Neighbor Totoro!"
 
 //2. There is a "sleep" study going on at a clinic and they are working with 5 study-subjects: "Joe", "Jane"; "lila"; "Pepe" and  "Gina". Write a function named "hoursSleep" with an "if-else statement" that prints who sleeps more hours each day once doctors pass the data of the hours each subject slept. It should run like this:
 // example: 
 //hoursSleep(4, 5, 8, 9, 7); // -->> "Pepe slept today the most! 9 hours"
 
-   function hoursSleep(hoursJoe, hoursJane, hoursLila, hoursPepe, hoursGina) {
-      if (hoursJoe > hoursJane && hoursJoe > hoursLila && hoursJoe > hoursPepe && hoursJoe > hoursGina)
-   {
-       console.log(`Joe slept today the most! ${hoursJoe} hours`);
-       }
-       else if (hoursJane > hoursJoe && hoursJane > hoursLila && hoursJane > hoursPepe && hoursJane > hoursGina)
-       {
-           console.log(`Jane slept today the most! ${hoursJane} hours`);
-       }
-       else if (hoursLila > hoursJoe && hoursLila > hoursJane && hoursLila > hoursPepe && hoursLila > hoursGina)
-       {
-           console.log(`Lila slept today the most! ${hoursLila} hours`);
-       }
-       else if (hoursPepe > hoursJoe && hoursPepe > hoursLila && hoursPepe > hoursJane && hoursPepe > hoursGina)
-       {
-           console.log(`Pepe slept today the most! ${hoursPepe} hours`);
-       }
-       else
-       {
-           console.log(`Gina slept today the most! ${hoursGina} hours`);
-       }
 
-   }
-hoursSleep(4, 5, 8, 9, 7); 
-// it prints: Pepe slept today the most! 9 hours
    
 //3. // Create a multiple case switch program where you can write different food items from the supermarket and it will print if they are fruits or not. ( hint: a list of fruit cases must be therefore included...)
 // example: 
 // "banana is a real fruit!";
 // "Potato is not a fruit" ;       
 
-let fruit = 'banana';
-switch(fruit) {
-    case "apple":
-    case "mango":
-    case "pineapple":
-    case "tomato":
-    case "banana":
-      console.log(`${fruit} is a real fruit!`);
-        break;
-    default:
-        console.log(`${fruit} is not a fruit!!`);
-        break;
-}
-//it prints: banana is a real fruit!
-
-//4. Create a javascript program with switch statement where you write the name of a city and it prints something that city has. ex: "San Francisco has the Golden Gate".
-let city = "Malaga";
-
-switch (city){
-    case "Malaga":
-        console.log("There are Palm trees in Malaga");
-        break;
-    case "Roma":
-        console.log("Roma has the Colosseo");
-        break;
-    case "San Francisco":
-        console.log("San Francisco has the Golden Gate");
-        break;
-    default:
-        console.log("Please select another city");
-}
-//it prints: There are Palm trees in Malaga
 
 
-// 5. Write a loop (using 2 arrays as variables), that prints every weekday menu like this: "MENU: Monday, we have pizza for lunch at the canteen"
 
-let weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-let food = ["pizza", "beans-pot", "veggie-tacos", "salad and fish", "pasta"];
+//4. Create a javascript program with switch statement where you write the name of a city and it prints something that city has. //ex: "San Francisco has the Golden Gate".
+//ex: There are Palm trees in Malaga
 
-for( i = 0 ; i < weekday.length ; i++){
-    console.log(`MENU: ${weekday[i]}, we have ${food[i]} for lunch at the canteen`)};
 
-// It prints:    
+// 5. Write a loop (using 2 arrays as variables), that prints every weekday menu like this: 
+   
 // MENU: Monday, we have pizza for lunch at the canteen
 // MENU: Tuesday, we have beans-pot for lunch at the canteen
 // MENU: Wednesday, we have veggie-tacos for lunch at the canteen
@@ -113,45 +46,14 @@ for( i = 0 ; i < weekday.length ; i++){
 // ^^^^^^^^^
 
 
-let x , y, character = ("");
-for( x = 1; x <= 10; x++)
-{
-   for (y = 1; y < x; y++)
-     {
-    character = character +("^");        
-      }
-    console.log(character);
-    character = '';    
-}
 
 //7. Create a function, give it the name "result". It passes 2 integer numbers as arguments. Calculate the sum of the two given integers. If the two values are the same, then print triple their sum. 
 
-function result(number1, number2) {
-    if (number1 === number2) {
-        return (number1 + number2) * 3;
-    } else {
-        return number1 + number2;
-    }
-}
-console.log(result(10, 10)); //prints 60
 
 
-//8.Write a function that takes a string and prints its length. example: it should print "The length of "Wooopiwoo" is: 9"; Write the same functions as an arrow function.
-function stringLength(string){
-    let length = string.length;
-    console.log(`The length of "${string}" is:`, length);
-    return length;
-}
+//8.Write a function that takes a string and prints its length. Write the same functions as an arrow function.
 
-stringLength("Wooopiwoo"); //The length of "Wooopiwoo" is: 9
-
-
-const stringLength1 = string => {
-    let length = string.length;
-    console.log(`The length of "${string}" is:`, length);
-    return length;
-}
-stringLength("Hello, how are you?");//The length of "Hello, how are you?" is: 19
+//stringLength("Hello, how are you?");//The length of "Hello, how are you?" is: 19
 
 //  9. A fashion brand has designed a unisex jacket and it is going into production.If each size requires 20 cm more of fabric and the prototype "size M" requires 1.8 m. How many fabric rolls do we need to produce 50 jackets of each size, given that each roll has 40 meters. Sizes in production are XS, S, M, L, XL. Round meter numbers and remember production can only buy full rolls!
 
@@ -170,53 +72,6 @@ stringLength("Hello, how are you?");//The length of "Hello, how are you?" is: 19
 // **BONUS 2: How many rolls do we need if the fabric is double width? Print it as a string and specify how many rolls are lining and how many are main fabric.
 
 
-function metersOfFabric() {
-     const sizes = {
-           size_XS: (1.8 - 0.4),
-           size_S: (1.8 - 0.2),
-           size_M: 1.8,
-           size_L: 1.8 + 0.2,
-           size_XL: 1.8 + 0.4,
-    
-     }
-    const metersPerSize = {
-        XS: (sizes.size_XS * 50),
-        S: (sizes.size_S * 50),
-        M: (sizes.size_M * 50),
-        L: (sizes.size_L * 50),
-        XL: Math.round((sizes.size_XL * 50)),
-    } 
-    //this is one way to calculate the total of meters needed:
-    // const totalFabricNeeded = Object.values(metersPerSize).reduce((a, b) => a + b, 0); 
-    
-    //this is other way to calculate the total of meters needed:
-    let totalFabricNeeded = sum( metersPerSize );
-    function sum( obj ) {
-        let sum = 0;
-        for( let metersEachSize in obj ) {
-          if( obj.hasOwnProperty( metersEachSize ) ) {
-            sum += parseFloat( obj[metersEachSize] );
-          }
-        }
-        return sum;
-      }
-      
-      const totalFabricRolls = Math.ceil(totalFabricNeeded / 40); 
-      
-      const fabricForLining = (totalFabricNeeded * 0.8);
-      const totalLiningFabricRolls = Math.ceil(fabricForLining / 50);
-      
-        
-      //console.log(sizes);//HELPER LOG
-      //console.log(metersPerSize);//HELPER LOG
-      //console.log( "sum of meters needed: "+ totalFabricNeeded );//HELPER LOG
-        console.log("The amount of fabric rolls needed is " + totalFabricRolls);
-      //console.log( "sum of lining meters needed: "+ fabricForLining);//HELPER LOG
-        console.log("The amount of lining fabric rolls needed is " + totalLiningFabricRolls);
-        console.log(`If fabric is double width: all jackets in production require ${totalFabricRolls / 2} rolls of fabric and ${totalLiningFabricRolls / 2} rolls of lining.`);
-    };
-metersOfFabric();
-
 
 // 10- Solving problems with JavaScript (problems like we have had, such as counting sports scores or censoring words)
 
@@ -230,15 +85,8 @@ metersOfFabric();
 // which shift sold best on the best day selling sandwiches?
 // which was the worst day to sell sandwiches?
 
-//I have to still write the Solution for this one ! so not done yet.
 
-const SandwichesSold = {
-    morningShift:  [15,20,25,35,45,100,50],
-    afternoonShift:  [15,20,25,35,45,100,50],
-    eveningShift: [15,20,25,35,45,100,50],
-    
 
-};
 
 
   
